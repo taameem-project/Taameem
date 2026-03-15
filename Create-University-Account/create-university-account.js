5be42c3fa9cd1a9ace71f84b5c32f429b9de146e
Almuntasar25 <almuntasar.x@email.com>
2026-03-15 13:05:37 +0400

    edit pag and add create-university-account

--- Create-University-Account/create-university-account.js
+++ Create-University-Account/create-university-account.js
@@ -1,3 +1,20 @@
+
+const accountButtons = document.querySelectorAll(".account-switch button")
+
+accountButtons.forEach(btn => {
+
+btn.addEventListener("click",()=>{
+
+accountButtons.forEach(b=>b.classList.remove("active"))
+
+btn.classList.add("active")
+
+})
+
+})
+
+
+
 const tabs = document.querySelectorAll(".tab")
 
 tabs.forEach(tab=>{
--- create-ministry-account/create-ministry-account.js
+++ create-ministry-account/create-ministry-account.js
@@ -1,3 +1,17 @@
+const accountButtons = document.querySelectorAll(".account-switch button")
+
+accountButtons.forEach(btn => {
+
+btn.addEventListener("click",()=>{
+
+accountButtons.forEach(b=>b.classList.remove("active"))
+
+btn.classList.add("active")
+
+})
+
+})
+
 const tabs = document.querySelectorAll(".tab")
 
 tabs.forEach(tab=>{
@@ -11,3 +25,4 @@ tab.classList.add("active")
 })
 
 })
+
